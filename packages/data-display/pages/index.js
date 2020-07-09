1152cad080de2ff5279232cc05005d333aeb3258
Michał <skorzecmichal@gmail.com>
2020-07-09 13:08:45 +0200

    branch cleanup #2

--- packages/data-display/pages/index.js
+++ packages/data-display/pages/index.js
@@ -1,54 +1,24 @@
 import Fetch from 'isomorphic-unfetch';
 import React from 'react';
 import TableVirtualized from '../components/TableVirtualized';
+import { Product } from '@bushidogames/db';
 
 class Root extends React.Component {
   static async getInitialProps(ctx) {
-    const res = await Fetch('http://localhost:3000/api/getProducts');
-    const data = await res.json();
+    const data = await Product.findAll();
+    // const res = await Fetch('http://localhost:3000/api/getProducts');
+    // const data = await res.json();
     return {
       products: data,
     };
   }
 
-  columns = [
-    {
-      Header: 'ID',
-      accessor: 'id',
-    },
-    {
-      Header: 'Service',
-      accessor: 'service',
-    },
-    {
-      Header: 'Producer',
-      accessor: 'producer',
-    },
-    {
-      Header: 'Name',
-      accessor: 'name',
-    },
-    {
-      Header: 'EAN',
-      accessor: 'ean',
-    },
-    {
-      Header: 'Price',
-      accessor: 'price',
-    },
-    {
-      Header: 'CeneoPrice',
-      accessor: 'ceneoPrice',
-    },
-  ];
-
   render() {
     const { products } = this.props;
-    const { columns } = this;
 
     return (
       <>
-        <TableVirtualized data={products} columns={columns} />
+        <TableVirtualized data={products} />
       </>
     );
   }
