1cafc605be50026f777f828660c1deb0b7546ba3
Wojciech Skowronek <wojciech.skowronek@toptal.com>
2020-07-10 12:43:44 +0200

    setup docker for hutzoo-parser package

--- packages/db/index.mjs
+++ packages/db/index.mjs
@@ -1,9 +1,8 @@
 'use strict';
 import dotenv from 'dotenv';
-import path from 'path';
 import Sequelize from 'sequelize';
-import { ProductModel } from './models/product.js';
-import { CategoryModel } from './models/category.js';
+import {ProductModel} from './models/product.js';
+import {CategoryModel} from './models/category.js';
 
 dotenv.config();
 
@@ -14,7 +13,7 @@ export const sequelize = new Sequelize(
   {
     logging: false,
     port: process.env.MYSQL_PORT,
-    host: '127.0.0.1',
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql',
     // todo: setup pool for prod
     // pool: {
